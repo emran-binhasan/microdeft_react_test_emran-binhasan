83e39d6ea1810ba76cfe06000f78063a6f2d6992
emran-binhasan <emranbinhasan.zero@gmail.com>
2024-12-27 23:52:26 +0600

    Add Login  form components and implement POST method for user

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
-
-export default Login;
+  const [formData, setFormData] = useState({ email: '', password: '' });
+    const handleLogin = async (event) => {
+      event.preventDefault();
+      try {
+        const response = await fetch('https://react-interview.crd4lc.easypanel.host/api/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData),
+        });
+        const res = await response.json();
+        if (response.ok) {
+          localStorage.setItem('token',res.data.token)
+          alert('Login successful');
+        };
+    } catch (error) {
+        alert('Login Failed')
+    }
+  }
+    return (
+      <div className="container">
+        <form onSubmit={handleLogin}>
+          <h2 className="registerTitle">Login to your account</h2>
+          <input type="email" placeholder="Email" required onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
+          <input type="password" placeholder="Password" required onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  };
+  
+  export default Login;
+  
+    
+  
+   
